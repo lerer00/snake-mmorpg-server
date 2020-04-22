45cf7664e8ca50e2e8e7bef7711f752c6bf6f78c
Francis Boily <francis.boily@axesnetwork.com>
2020-04-21 23:19:09 -0400

    Updating log.

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ const app = http.createServer()
 const io = socket(app);
 
 app.listen(process.env.PORT || 3000);
+console.log("Listening on port " + (process.env.PORT || 3000));
 
 // Single game server.
 const game = new Game();
